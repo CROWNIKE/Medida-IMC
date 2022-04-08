17c9d3bc1ab2aa3a1ee846cb242d4797c89ada11
NAYCKERSON PEDRO <99208486+CROWNIKE@users.noreply.github.com>
2022-04-08 10:39:10 -0300

    Add project files.

---
+++ Pessoa.console/Pessoa.cs
@@ -0,0 +1,63 @@
+﻿namespace Pessoa.console
+{
+    internal class Pessoa
+    {
+        //Atributos
+        public double peso, altura;
+
+        //Cálculo
+        public double calculo()
+        {
+            return peso / (altura * altura);
+
+        }
+
+        //Situacao
+
+        public string Situacao(double imc)
+        {
+            //Variavel
+            string retorno;
+
+            //Condicional
+            if (imc < 18.5)
+            {
+                retorno = "Abaixo do peso";
+
+            }
+            else if (imc < 25)
+            {
+                retorno = "Peso normal";
+            }
+            else if (imc < 30)
+            {
+                retorno = "Acima do peso";
+            }
+            else if (imc < 35)
+            {
+                retorno = "Obesidade 1";
+            }
+            else if (imc < 40)
+            {
+                retorno = "Obesidade 2";
+            }
+            else
+            {
+                retorno = "Obesidade 3";
+            }
+            //Retorno
+            return retorno;
+        }
+    }
+    //Mensagem
+    public void Mensagem()
+    {
+        //Obter o calculo
+        double obterCalculo = Calculo();
+
+        //Obter situacao
+        string obterSituacao = Situacao(obterCalculo);
+
+        //Exibir 
+    }
+}
